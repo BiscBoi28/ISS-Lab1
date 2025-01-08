b954dc3f5b5b27230be0a5a39a982a4f57f2888b
BiscBoi28 <ayush.aga@students.iiit.ac.in>
2025-01-08 14:46:59 +0530

    meow meow

---
+++ GYATT.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<point.h>
+#include<distance.c>
+int main(){
+    
+}
---
+++ distance.c
@@ -0,0 +1,5 @@
+#include<math.h>
+
+double dist(int x1,int x2, int y1, int y2){
+    return sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
+}   
